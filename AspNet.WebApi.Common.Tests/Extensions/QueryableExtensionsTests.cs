b4c76bb9b8fefe80da82f61251ca697ca7ef09fd
Leonid Efremov <leonid.efremov@me.com>
2019-02-19 13:10:11 +0300

    QueryableExtensions added

---
+++ AspNet.WebApi.Common.Tests/Extensions/QueryableExtensionsTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using AspNet.WebApi.Common.Extensions;
+using Xunit;
+
+namespace AspNet.WebApi.Common.Tests.Extensions
+{
+    public class QueryableExtensionsTests
+    {
+        [Fact]
+        public void Where()
+        {
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }.AsQueryable();
+
+            var actual = data.Where(_ => _ < 5, true).ToArray();
+            Assert.Equal(new[] { 1, 2, 3, 4, 0 }, actual);
+
+            actual = data.Where(_ => _ < 5, false).ToArray();
+            Assert.Equal(data, actual);
+        }
+
+        [Fact]
+        public void PageBy()
+        {
+            var data = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }.AsQueryable();
+
+            var actual = data.PageBy(1, 2).ToArray();
+            Assert.Equal(new[] { 2, 3 }, actual);
+        }
+    }
+}
---
+++ AspNet.WebApi.Common/Extensions/QueryableExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace AspNet.WebApi.Common.Extensions
+{
+    /// <summary>Queryable Extensions.</summary>
+    public static class QueryableExtensions
+    {
+        /// <summary>Apply Where by condition.</summary>
+        /// <typeparam name="T">Base Type.</typeparam>
+        /// <param name="query">Query.</param>
+        /// <param name="predicate">Predicate.</param>
+        /// <param name="condition">Condition.</param>
+        /// <returns><see cref="IQueryable{T}"/>.</returns>
+        public static IQueryable<T> Where<T>(this IQueryable<T> query, Expression<Func<T, bool>> predicate, bool condition) => condition ? query.Where(predicate) : query;
+
+        /// <summary>Apply paging.</summary>
+        /// <typeparam name="T">Base Type.</typeparam>
+        /// <param name="query">Query.</param>
+        /// <param name="skipCount">Skip count.</param>
+        /// <param name="takeCount">Take count.</param>
+        /// <returns><see cref="IQueryable{T}"/>.</returns>
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> query, int skipCount, int takeCount) => query.Skip(skipCount).Take(takeCount);
+    }
+}
